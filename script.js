cf79ed0d908de826ec304843edfd746abf690742
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-11-08 22:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your script here.
+const btn = document.querySelector("#btn")
+const countDown = document.querySelector("#countDown")
+const endTime = document.querySelector("#endTime")
+btn.addEventListener("click", () => { 
+  const timer = document.querySelector("input")
+  const currentTime = new Date()
+  const futureTime = new Date(currentTime.getTime() + timer.value * 60000)
+  endTime.innerText = futureTime.toLocaleTimeString()
+  let time = timer.value
+  setInterval(() => {
+    countDown.innerText = --time
+  }, timer.value * 10000)
+})
